ed016ff165af6a7b3257867b552c78e9973757ab
Valentin Iuriet <valentin.iuriet@gmail.com>
2014-03-29 16:15:34 +0100

    Added Scores and buttons on main activity

--- gen/com/example/partyalias/R.java
+++ gen/com/example/partyalias/R.java
@@ -24,30 +24,39 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int btnInstructions=0x7f080001;
+        public static final int btnShare=0x7f080002;
+        public static final int btnStart=0x7f080000;
     }
     public static final class layout {
         public static final int activity_game_board=0x7f030000;
         public static final int activity_play_game=0x7f030001;
-        public static final int activity_start=0x7f030002;
-        public static final int activity_start_game1=0x7f030003;
-        public static final int activity_start_game2=0x7f030004;
+        public static final int activity_scores=0x7f030002;
+        public static final int activity_start=0x7f030003;
+        public static final int activity_start_game1=0x7f030004;
+        public static final int activity_start_game2=0x7f030005;
     }
     public static final class menu {
         public static final int game_boar=0x7f070000;
         public static final int game_board=0x7f070001;
         public static final int play_game=0x7f070002;
-        public static final int start=0x7f070003;
-        public static final int start_game1=0x7f070004;
-        public static final int start_game2=0x7f070005;
+        public static final int scores=0x7f070003;
+        public static final int start=0x7f070004;
+        public static final int start_game1=0x7f070005;
+        public static final int start_game2=0x7f070006;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int instructions=0x7f050009;
+        public static final int share=0x7f05000a;
+        public static final int start=0x7f050008;
         public static final int title_activity_game_boar=0x7f050005;
         public static final int title_activity_game_board=0x7f050006;
         public static final int title_activity_play_game=0x7f050007;
+        public static final int title_activity_scores=0x7f05000b;
         public static final int title_activity_start_game1=0x7f050003;
         public static final int title_activity_start_game2=0x7f050004;
     }
