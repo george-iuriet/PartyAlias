c4bf55294b848fe605d7719b8cfbc9b858a4ab4b
Valentin Iuriet <valentin.iuriet@gmail.com>
2014-03-29 15:47:31 +0100

    am adaugat toast

--- src/com/example/partyalias/StartActivity.java
+++ src/com/example/partyalias/StartActivity.java
@@ -3,6 +3,7 @@ package com.example.partyalias;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class StartActivity extends Activity {
 
@@ -10,6 +11,7 @@ public class StartActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start);
+		Toast.makeText(getBaseContext(), "gie", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
